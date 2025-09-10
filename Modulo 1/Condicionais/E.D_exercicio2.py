d4fe5bfec00c3d1568a7dddb196aed73e058ffae
Kaveg_cast <kcastroveiga@gmail.com>
2025-09-10 14:47:03 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/E.D_exercicio2.py
@@ -0,0 +1,7 @@
+valor = int(input("digite o valor"))
+
+
+if valor < -1:
+    print("negativo")
+else:
+    print("positivo")
---
+++ Modulo 1/Condicionais/E.D_exercicio3.py
@@ -0,0 +1,9 @@
+letra = input("digite uma letra: ")
+
+
+if letra == "f":
+    print("feminino")
+elif letra == "m":
+    print("masculino")
+else:
+    print("sexo invalido")
